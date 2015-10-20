68cc623a8255d6a79195fda8f50279af576101c3
Joel Denning <joeldenning@gmail.com>
2015-10-20 00:26:58 -0600

    Tabs to spaces, getting rid of weird binding, and catching all promise exceptions

--- src/single-spa-jspm.js
+++ src/single-spa-jspm.js
@@ -1,88 +1,105 @@
 export function defaultJspmApp(config = {}) {
-	let app = {};
-	app.scriptsWillBeLoaded = function() { return scriptsWillBeLoaded.apply(config, arguments); }
-	app.scriptsWereLoaded = function() { return scriptsWereLoaded.apply(config, arguments); }
-	app.applicationWillMount = function() { return applicationWillMount.apply(config, arguments); }
-	app.mountApplication = function() { return mountApplication.apply(config, arguments); }
-	app.applicationWasMounted = function() { return applicationWasMounted.apply(config, arguments); }
-	app.applicationWillUnmount = function() { return applicationWillUnmount.apply(config, arguments); }
-	app.applicationWasUnmounted = function() { return applicationWillUnmount.apply(config, arguments); }
-	app.activeApplicationSourceWillUpdate = function() { return activeApplicationSourceWillUpdate.apply(config, arguments); }
-	app.activeApplicationSourceWasUpdated = function() { return activeApplicationSourceWasUpdated.apply(config, arguments); }
-	return app;
+    let app = {};
+    app.scriptsWillBeLoaded = function() { return scriptsWillBeLoaded.apply(config, arguments); }
+    app.scriptsWereLoaded = function() { return scriptsWereLoaded.apply(config, arguments); }
+    app.applicationWillMount = function() { return applicationWillMount.apply(config, arguments); }
+    app.mountApplication = function() { return mountApplication.apply(config, arguments); }
+    app.applicationWasMounted = function() { return applicationWasMounted.apply(config, arguments); }
+    app.applicationWillUnmount = function() { return applicationWillUnmount.apply(config, arguments); }
+    app.applicationWasUnmounted = function() { return applicationWillUnmount.apply(config, arguments); }
+    app.activeApplicationSourceWillUpdate = function() { return activeApplicationSourceWillUpdate.apply(config, arguments); }
+    app.activeApplicationSourceWasUpdated = function() { return activeApplicationSourceWasUpdated.apply(config, arguments); }
+    return app;
 }
 
 export function scriptsWillBeLoaded() {
-	return new Promise(function (resolve, reject) {
-		this.nativeSystemGlobal = window.System;
-		delete window.System;
-		resolve()
-	}.bind(this));
+    let config = this;
+    return new Promise(function (resolve, reject) {
+        this.nativeSystemGlobal = window.System;
+        delete window.System;
+        resolve()
+    });
 }
 
 export function scriptsWereLoaded() {
-	return new Promise(function (resolve) {
-		this.appLoader = window.System;
-		switchToNativeLoader.call(this)
-		.then(() => resolve());
-	}.bind(this))
+    let config = this;
+    return new Promise(function (resolve) {
+        this.appLoader = window.System;
+        switchToNativeLoader.call(this)
+        .then(() => resolve())
+        .catch((ex) => {
+            throw ex;
+        });
+    })
 }
 
 export function applicationWillMount() {
-	return new Promise(function (resolve) {
-		switchToAppLoader.call(this)
-		.then(() => resolve());
-	}.bind(this));
+    let config = this;
+    return new Promise(function (resolve) {
+        switchToAppLoader.call(this)
+        .then(() => resolve())
+        .catch((ex) => {
+            throw ex;
+        });
+    });
 }
 
 export function applicationWasMounted() {
-	return new Promise(function(resolve) {
-		resolve()
-	}.bind(this));
+    let config = this;
+    return new Promise(function(resolve) {
+        resolve()
+    });
 }
 
 export function applicationWillUnmount() {
-	return new Promise(function(resolve) {
-		resolve()
-	}.bind(this));
+    let config = this;
+    return new Promise(function(resolve) {
+        resolve()
+    });
 }
 
 export function applicationWasUnmounted() {
-	return new Promise(function (resolve) {
-		switchToNativeLoader.call(this)
-		.then(() => resolve());
-	}.bind(this));
+    let config = this;
+    return new Promise(function (resolve) {
+        switchToNativeLoader.call(this)
+        .then(() => resolve())
+        .catch((ex) => {
+            throw ex;
+        });
+    });
 }
 
 export function activeApplicationSourceWillUpdate() {
-	return new Promise(function(resolve) {
-		resolve()
-	}.bind(this));
+    let config = this;
+    return new Promise(function(resolve) {
+        resolve()
+    });
 }
 
 export function activeApplicationSourceWasUpdated() {
-	return new Promise(function(resolve) {
-		resolve()
-	}.bind(this));
+    let config = this;
+    return new Promise(function(resolve) {
+        resolve()
+    });
 }
 
 var cachebuster = 0;
 
 function switchToAppLoader() {
-	return new Promise((resolve) => {
-		if (!this.appLoader) {
-			throw new Error(`cannot switch to this app's jspm loader -- no app loader exists`)
-		} else {
-			this.nativeSystemGlobal = window.System;
-			window.System = this.appLoader;
-			resolve();
-		}
-	})
+    return new Promise((resolve) => {
+        if (!this.appLoader) {
+            throw new Error(`cannot switch to this app's jspm loader -- no app loader exists`)
+        } else {
+            this.nativeSystemGlobal = window.System;
+            window.System = this.appLoader;
+            resolve();
+        }
+    })
 }
 
 function switchToNativeLoader() {
-	return new Promise((resolve) => {
-		window.System = this.nativeSystemGlobal;
-		resolve();
-	})
+    return new Promise((resolve) => {
+        window.System = this.nativeSystemGlobal;
+        resolve();
+    })
 }
